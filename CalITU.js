9e3ea77ebbb962b61233e4c4ff229cf0d17a930e
IgnatevA1 <a.ignatyev@proton-electrotex.com>
2024-05-17 19:50:49 +0300

    ITU 311

--- CalITU.js
+++ CalITU.js
@@ -1,18 +1,25 @@
 include('TestITU.js')
 include('CalGeneral.js')
+// Совместимость: 0 - версия 1.0.0, 1 - версия 1.0
+compatibility = 0
 
 // Доступные границы формирования напряжения (в В)
 citu_Vmin = 1000
-citu_Vmax = 8000
+citu_Vmax = 10500
 
 // Выбранный диапазон тока: 1 - старший, 2 - средний, 3 - младший
-citu_IRange = 1
+citu_IRange = 3
+
+// Выбор канала тока: 0 - 1 канал, 1 - 2 канал, 2 - 3 канал, 3 - 4 канал
+Channel = 3
 
 // Время считывания результата от готовности напряжения (в сек)
 citu_ProbeTime = 3
 
 // Сопротивление нагрузки для калибровки/верификации тока (в Ом)
-citu_LoadRes = 10e6
+//citu_LoadRes = 93880	// старший
+//citu_LoadRes = 10e5		// средний
+citu_LoadRes = 9.909e6	// младший
 
 // Хранилища результатов
 // Блока
@@ -91,7 +98,7 @@ function CITU_Current(Calibrate)
 	var VoltageValues = CGEN_GetRange(citu_Vmin, Vmax, VStep)
 	
 	if(Calibrate)
-		CITU_CalI(0, 1, 0)
+		CITU_CalI(Channel, 0, 1, 0)
 	
 	if(CITU_Collect(VoltageValues, CurrentParams.Limit, citu_Iterations))
 	{
@@ -101,8 +108,8 @@ function CITU_Current(Calibrate)
 		if(Calibrate)
 		{
 			var corr = CGEN_GetCorrection2('itu_i')
-			CITU_CalI(corr[0], corr[1], corr[2])
-			CITU_PrintCalI()
+			CITU_CalI(Channel, corr[0], corr[1], corr[2])
+			CITU_PrintCalI(Channel)
 		}
 	}
 }
@@ -111,7 +118,7 @@ function CITU_Collect(VoltageValues, MaxCurrent, IterationsCount)
 {
 	// Подготовительные настройки
 	CITU_PrepareMultimeter(MaxCurrent)
-	dev.w(132, 60)
+	dev.w(132, 10)
 	if(dev.r(192) == 0)
 		dev.c(1)
 	
@@ -192,15 +199,30 @@ function CITU_TestCallback(VoltageReady)
 		}
 		else
 		{
-			citu_i.push(unit_result.i.toFixed(3))
+			if(!compatibility)
+			{
+				if(Channel == 0)
+				unit_result.ix = unit_result.i
+				else if(Channel == 1)
+				unit_result.ix = unit_result.i2	
+				else if(Channel == 2)
+				unit_result.ix = unit_result.i3
+				else
+				unit_result.ix = unit_result.i4	
+			}	
+			else
+				unit_result.ix = unit_result.i
+
+			unit_result.i = unit_result.i2
+			citu_i.push(unit_result.ix.toFixed(3))
 			citu_i_ref.push(ref_result.toFixed(4))
-			citu_i_err.push(((unit_result.i - ref_result) / ref_result * 100).toFixed(2))
+			citu_i_err.push(((unit_result.ix - ref_result) / ref_result * 100).toFixed(2))
 		}
 		
 		dev.c(101)
 	}
 }
-
+//
 function CITU_PrepareMultimeter(MaxCurrent)
 {
 	tmc.co()
@@ -216,59 +238,101 @@ function CITU_PrepareMultimeter(MaxCurrent)
 		tmc.w('CURR:AC:RANG ' + (MaxCurrent / 1000))
 	}
 }
-
-function CITU_GetIRangeParameters()
+//+
+function CITU_GetIRangeParameters(Channel)
 {
-	switch(citu_IRange)
+	if(compatibility)
 	{
-		case 1:
-			return {StartReg : 9,  Limit : dev.r(215), P0div : 10}
+		switch(citu_IRange)
+		{
+			case 1:
+				return {StartReg : 9,  Limit : dev.r(215), P0div : 10}
 		
-		case 2:
-			return {StartReg : 14, Limit : dev.r(216), P0div : 100}
+			case 2:
+				return {StartReg : 14, Limit : dev.r(216), P0div : 100}
 		
-		case 3:
-			return {StartReg : 19, Limit : dev.r(217), P0div : 1000}
+			case 3:
+				return {StartReg : 19, Limit : dev.r(217), P0div : 1000}
+		}
+	}	
+	else
+	{
+		switch(citu_IRange)
+		{
+			case 1:
+				return {StartReg : 10 + (12 * Channel),  Limit : dev.rf(75)}
+		
+			case 2:
+				return {StartReg : 14 + (12 * Channel), Limit : dev.rf(74)}
+		
+			case 3:
+				return {StartReg : 18 + (12 * Channel), Limit : dev.rf(73)}
+		}		
 	}
 }
-
+//+
 function CITU_CalX(Params, P2, P1, P0)
 {
-	dev.ws(Params.StartReg, Math.round(P2 * 1e6))
-	dev.w(Params.StartReg + 1, Math.round(P1 * 1000))
-	dev.ws(Params.StartReg + 2, Math.round(P0 * Params.P0div))
-}
-
+	if(compatibility)
+	{	
+		dev.ws(Params.StartReg, Math.round(P2 * 1e6))
+		dev.w(Params.StartReg + 1, Math.round(P1 * 1000))
+		dev.ws(Params.StartReg + 2, Math.round(P0 * Params.P0div))
+	}
+	else
+	{
+		dev.wf(Params.StartReg, P2)
+		dev.wf(Params.StartReg + 1, P1)
+		dev.wf(Params.StartReg + 2, P0)
+	}
+}	
+//+
 function CITU_CalV(P2, P1, P0)
 {
-	CITU_CalX({StartReg : 4, P0div : 10}, P2, P1, P0)
+	if(compatibility)
+		CITU_CalX({StartReg : 4, P0div : 10}, P2, P1, P0)
+	else
+		CITU_CalX({StartReg : 6}, P2, P1, P0)
 }
-
-function CITU_CalI(P2, P1, P0)
+//+
+function CITU_CalI(Channel, P2, P1, P0)
 {
-	var Params = CITU_GetIRangeParameters()
+	var Params = CITU_GetIRangeParameters(Channel)
 	if(Params)
 		CITU_CalX(Params, P2, P1, P0)
 }
-
+//+
 function CITU_PrintCalX(Title, Params)
 {
-	p(Title)
-	p('P2 x1e6 : ' + dev.rs(Params.StartReg))
-	p('P1 x1000: ' + dev.r(Params.StartReg + 1))
-	p('P0 x' + Params.P0div + (Params.P0div == 100 ? ' ' : Params.P0div == 10 ? '  ' : '') +
-		': ' + dev.rs(Params.StartReg + 2))
+	if(compatibility)
+	{
+		p(Title)
+		p('P2 x1e6 : ' + dev.rs(Params.StartReg))
+		p('P1 x1000: ' + dev.r(Params.StartReg + 1))
+		p('P0 x' + Params.P0div + (Params.P0div == 100 ? ' ' : Params.P0div == 10 ? '  ' : '') +
+			': ' + dev.rs(Params.StartReg + 2))
+	}
+	else
+	{
+		p(Title)
+		p('P2 : ' + dev.rf(Params.StartReg))
+		p('P1 : ' + dev.rf(Params.StartReg + 1))
+		p('P0 : ' +  dev.rf(Params.StartReg + 2))
+	}	
 }
-
+//+
 function CITU_PrintCalV()
 {
-	CITU_PrintCalX('Voltage', {StartReg : 4, P0div : 10})
+	if(compatibility)
+		CITU_PrintCalX('Voltage', {StartReg : 4, P0div : 10})
+	else
+		CITU_PrintCalX('Voltage', {StartReg : 6})
 }
-
-function CITU_PrintCalI()
+//+
+function CITU_PrintCalI(Channel)
 {
-	var Params = CITU_GetIRangeParameters()
+	var Params = CITU_GetIRangeParameters(Channel)
 	if(Params)
 		CITU_PrintCalX('Current range ' + citu_IRange +
-			' up to ' + Params.Limit + ' mA', Params)
+			' up to ' + Params.Limit + ' mA ' + (Channel + 1) + ' Channel' , Params)
 }
