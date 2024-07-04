88e0e798a68e13ade963ce7bb9db172f4844b111
Pumukun <demgrisha@gmail.com>
2024-07-04 19:02:10 +0300

    Переписал функцию чтения из флеша, немного поправил скрипт

--- FlashRead.js
+++ FlashRead.js
@@ -1,15 +1,18 @@
-var ACT_FLASH_WRITE			= 332;	// Flash write
-var ACT_FLASH_ERASE			= 334;	// Flash erase data sector
-var ACT_FLASH_ARRAY_PUSH	= 335;	// Add value from REG_FLASH_WRITE_DATA to array
-var ACT_FLASH_ARRAY_CLEAR	= 336;	// Clear flash write buffer
+const ACT_FLASH_WRITE		= 332;	// Flash write
+const ACT_FLASH_ERASE		= 334;	// Flash erase data sector
+const ACT_FLASH_ARRAY_PUSH	= 335;	// Add value from REG_FLASH_WRITE_DATA to array
+const ACT_FLASH_ARRAY_CLEAR	= 336;	// Clear flash write buffer
 
-var ACT_READ_SYMBOL			= 330;	// Flash read symbol and shift
-var ACT_SELECT_MEM_LABEL	= 331;	// Flash read start position
+const ACT_READ_SYMBOL		= 330;	// Flash read symbol and shift
+const ACT_SELECT_MEM_LABEL	= 331;	// Flash read start position
 
-var REG_FLASH_WRITE_DATA	= 184;	// Flash temporary data register
-var REG_FLASH_WRITE_TYPE	= 185;	// Flash data type
+const REG_FLASH_WRITE_DATA	= 184;	// Flash temporary data register
+const REG_FLASH_WRITE_TYPE	= 185;	// Flash data type
 
-var REG_MEM_SYMBOL			= 299;	// Current data
+const REG_MEM_SYMBOL		= 299;	// Current data
+
+const FLASH_START			= 0x3D0000
+const FLASH_END				= 0x3E0000
 
 
 function flash_read()
@@ -22,8 +25,6 @@ function TypeLength(Type)
 {
 	return (Type > 4 ? 2 : 1)
 }
-
-// Структура блока: DataType, Length, Data
 
 /*
  * Type: number 
@@ -41,6 +42,20 @@ function FlashWrite(Data)
 	dev.c(ACT_FLASH_WRITE);
 }
 
+function FlashReadDescription()
+{
+	var Description = "";
+	var length = flash_read();
+	Description += length;
+
+	for (var i = 0; i < length; i++)
+	{
+		Description += String.fromCharCode(flash_read());
+	}
+
+	return Description;
+}
+
 /*
  * @return Array<any>
 */ 
@@ -48,29 +63,40 @@ function FlashReadAll()
 {
 	dev.c(ACT_SELECT_MEM_LABEL);
 
-	var tmpData = "";
+	var Data = "";
 
 	var DataArray = [];
 
-	for (var sp = 0x3D0000; sp < 0x3E0000; sp++)
+	for (var sp = FLASH_START; sp < FLASH_END; sp++)
 	{
-		var tmpDataType = flash_read();
-		tmpData += tmpDataType + " ";
+		var dataType = flash_read();
+		Data += dataType + " ";
 
-		if (tmpDataType > 7)
+		if (dataType > 7)
 			break;
 
+		if (dataType == 0)
+		{
+			var Description = "";
+			var length = flash_read();
+			Description += length;
+
+			for (var i = 0; i < length; i++)
+			{
+				Description += String.fromCharCode(flash_read());
+			}
+		}
+
 		var tmpLength = flash_read();
-		tmpData += tmpLength + " ";
+		Data += tmpLength + " ";
 
 		for (var i = 0; i < tmpLength; i++)
 		{
-			tmpData += flash_read() + " ";
+			Data += flash_read() + " ";
 		}
 
-		DataArray.push(tmpData);
-		p(tmpData);
-		tmpData = "";
+		DataArray.push(Data);
+		Data = "";
 	}
 
 	return DataArray;
@@ -85,7 +111,7 @@ function FlashRead(i)
 	}
 }
 
-function FlashEraseDataSector()
+function FlashErase()
 {
 	dev.c(ACT_FLASH_ERASE);
 }
