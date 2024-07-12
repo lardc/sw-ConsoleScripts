92287cf720879b9952f9381a45c7533870340296
Pumukun <demgrisha@gmail.com>
2024-07-12 20:42:02 +0300

    Добавил вывод считываеммых данных, удалил ненужный код

--- FlashRead.js
+++ FlashRead.js
@@ -1,18 +1,10 @@
 var ACT_FLASH_WRITE			= 332;	// Flash write
 var ACT_FLASH_ERASE			= 334;	// Flash erase data sector
-var ACT_FLASH_ARRAY_PUSH	= 335;	// Add value from REG_FLASH_WRITE_DATA to array
-var ACT_FLASH_ARRAY_CLEAR	= 336;	// Clear flash write buffer
 
 var ACT_READ_SYMBOL			= 330;	// Flash read symbol and shift
 var ACT_SELECT_MEM_LABEL	= 331;	// Flash read start position
 
-var REG_FLASH_WRITE_DATA	= 184;	// Flash temporary data register
-var REG_FLASH_WRITE_TYPE	= 185;	// Flash data type
-
 var REG_MEM_SYMBOL			= 299;	// Current data
-
-var FLASH_START				= 0x3D0000;
-var FLASH_END				= 0x3E7FFF;
 
 function flash_read()
 {
@@ -34,16 +26,16 @@ function FlashReadAll(PrintPlot)
 {
 	dev.c(ACT_SELECT_MEM_LABEL);
 
-	var Data = [];
-
-	var FileName = "";
-
-	for (var sp = FLASH_START; sp < FLASH_END; sp++)
+	while (true)
 	{
 		var dataType = flash_read();
 
 		if (dataType > 7)
 			break;
+
+		var Data = [];
+		var FileName = "";
+		var Message = "";
 
 		if (dataType == 0)
 		{
@@ -55,14 +47,17 @@ function FlashReadAll(PrintPlot)
 				Description += String.fromCharCode(flash_read());
 			}
 			FileName += Description;
+			Message += Description;
 		}
 		else
 		{
 			var length = flash_read();
+			Message += " (Type: Int16U, Length: " + length + ")\n";
 
 			for (var i = 0; i < length; i++)
 			{
 				Data.push(flash_read());
+				Message += Data[i];
 			}
 
 			if (Data.length > 1)
@@ -73,9 +68,8 @@ function FlashReadAll(PrintPlot)
 				if (PrintPlot)
 					pl(Data);
 			}
-			FileName = "";
-			Data = [];
 		}
+		p(Message);
 	}
 }
 
