da63cccf2c742f7bce138cc9262a6a9889e5b415
mlhzv2204 <r.malkhozov@proton-electrotex.com>
2024-08-13 09:58:29 +0300

    Учет кол-ва CUOutputExtensionBoard для тест пинов

--- TestCU.js
+++ TestCU.js
@@ -1,12 +1,14 @@
 include("PrintStatus.js")
 
+var Count_CUOutputExtensionBoard = 2; // Кол-во ЭМ CUOutputExtensionBoard в блоке
+
 function CU_SetPin(PinID, Action, ResetMode)
 {
 	// clear all pins
 	if (ResetMode != 0)
 	{
 		dev.w(67, 0);
-		for (i = 0; i < 3; i++)
+		for (i = 0; i < Count_CUOutputExtensionBoard; i++)
 		{
 			dev.w(66, i);
 			dev.c(103);
@@ -21,7 +23,6 @@ function CU_SetPin(PinID, Action, ResetMode)
 	dev.c(102);
 	dev.c(104);
 }
-
 
 function CU_TestInd (repeat)
 {
