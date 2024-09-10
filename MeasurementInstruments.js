350ecb4cc3803b8642134946207fff2969cd78b0
Pumukun <demgrisha@gmail.com>
2024-09-10 20:27:54 +0300

    Сделал функцию опроса портов для поиска осциллографа

--- MeasurementInstruments.js
+++ MeasurementInstruments.js
@@ -1,7 +1,13 @@
 include("Tektronix.js")
 include("DMM6500.js")
 
-function InitMeasurementInstrument()
+function InitMeasurementInstrument(COMports)
 {
-
+	for (var i = 0; COMports.length; ++i)
+	{
+		TEK_PortInit(COMports[i]);
+		var result = TEK_Exec("ID?");
+		if (result.includes("Tektronix"))
+			return i;
+	}
 }
