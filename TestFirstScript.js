4c0605b2481c81fb84062add5fbfa2d75a963446
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-01-15 12:23:03 +0300

    создан тестовый скрипт(TestFirstScript.js)который выводит в консоль, на график и в текстовый файл числа от 1 до 100, кратные 3 или 5.

---
+++ TestFirstScript.js
@@ -0,0 +1,21 @@
+function test_script()
+{
+	var ResultArray= [];
+	FirstTestNumber = 1;
+	LastTestNumber = 100;
+	
+	//поиск элементов
+	for (var i=FirstTestNumber; i<=LastTestNumber; i++)
+	{
+		if((i%3 == 0)||(i%5==0))
+			{
+				ResultArray.push(i);
+			}
+	}
+
+	var StringOutput= ResultArray.toString();
+
+	p(StringOutput);
+	pl(ResultArray);
+	save('Test_script_result.txt',StringOutput);
+}
