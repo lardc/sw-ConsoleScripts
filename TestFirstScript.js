d13dd5a9e32b412cce70d1c991f7c9657e4a406f
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-01-15 14:27:33 +0300

    исправлены замечания

--- TestFirstScript.js
+++ TestFirstScript.js
@@ -1,21 +1,21 @@
 function test_script()
 {
 	var ResultArray= [];
-	FirstTestNumber = 1;
-	LastTestNumber = 100;
+	var FirstTestNumber = 1;
+	var LastTestNumber = 100;
 	
 	//поиск элементов
-	for (var i=FirstTestNumber; i<=LastTestNumber; i++)
+	for (var i = FirstTestNumber; i <= LastTestNumber; i++)
 	{
 		if((i%3 == 0)||(i%5==0))
-			{
-				ResultArray.push(i);
-			}
+		{
+			ResultArray.push(i);
+		}
 	}
 
 	var StringOutput= ResultArray.toString();
 
 	p(StringOutput);
 	pl(ResultArray);
-	save('Test_script_result.txt',StringOutput);
+	save('Test_script_result.txt', StringOutput);
 }
