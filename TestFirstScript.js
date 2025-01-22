0c01a8cc8320470005f9b9bc06df59c575822fa8
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-01-22 14:20:34 +0300

    Исправлены замечания по поводу пробелов.

--- TestFirstScript.js
+++ TestFirstScript.js
@@ -23,12 +23,12 @@ function test_script()
 function study_abc_calc(port,param_A,param_B,param_C)
 {
 	dev.co(port);
-	dev.w(60,param_A);
-	dev.w(130,param_B);
-	dev.w(131,param_C);
+	dev.w(60, param_A);
+	dev.w(130, param_B);
+	dev.w(131, param_C);
 	dev.c(60);
 	dev.c(200);
-	p("("+dev.r(60)+"-"+dev.r(130)+")"+"/"+dev.r(131)+"="+dev.r(210));
+	p("(" + dev.r(60)+ " - " + dev.r(130)+ ")" + " / " + dev.r(131) + " = " + dev.r(210));
 }
 
 function study_ssm()
