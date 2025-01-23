4c41a3648036d92d1865787342e6ad630194f0aa
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-01-23 15:20:05 +0300

    в машину состояний добавлена возможность отслеживания времени состояний вкл и выкл лампочки.

--- TestFirstScript.js
+++ TestFirstScript.js
@@ -35,15 +35,46 @@ function study_ssm()
 {
 	var delay = 200;
 	
+	
 	p("Current state of LED - " + dev.r(211));
 	p("Current state of SSM - " + dev.r(212));
 	dev.c(62);
+	var Time_compare_param = Date.now(); 
+	var LED_State_save = 0; // доп параметр, чтобы if внутри case не срабатывал по неск раз.
 	while(dev.r(212) != 0)
 	{
-		p("Current state of LED - " + dev.r(211));
-		p("Current state of SSM - " + dev.r(212));
-		sleep(delay);
+		switch (dev.r(212))
+		{
+			case 2:
+				if (LED_State_save == 0)
+				{
+					LED_State_save = 1;
+					Time_compare = Date.now();
+				}
+				break;
+				
+			case 3:
+				if (dev.r(211) == 0 && LED_State_save == 1)
+				{
+					p("Время горения = " + (Date.now() - Time_compare));
+					LED_State_save = 0;
+					Time_compare = Date.now()
+				}
+				break;
+			case 4:
+			if (dev.r(211) == 1 && LED_State_save == 0)
+				{
+					p("Время гаснутое = " + (Date.now() - Time_compare));
+					LED_State_save = 1;
+					Time_compare = Date.now()
+				}
+				break;
+		}
+		//p("Current state of LED - " + dev.r(211));
+		//p("Current state of SSM - " + dev.r(212));
+		//sleep(delay);
 	}
+	p("Время горения = " + (Date.now() - Time_compare))
 	p("Current state of LED - " + dev.r(211));
 	p("Current state of SSM - " + dev.r(212));
 }
