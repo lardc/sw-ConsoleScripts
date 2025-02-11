cbef8b0a5f379d7f10254c7cdba0b705bc05c2f1
mlhzv2204 <r.malkhozov@proton-electrotex.com>
2025-02-11 18:36:00 +0300

    Убран функционал dVdt из SiC

--- SiC_GetData.js
+++ SiC_GetData.js
@@ -1,7 +1,5 @@
 include("Tektronix.js")
 
-sic_measuring_device = "TPS2024";	// "TPS2014"
-
 sic_gd_filter_points = 5;
 sic_gd_filter_factor = 0.5;
 
@@ -10,18 +8,9 @@ sic_gd_ice_shunt = 2.5e-3;
 
 function SiC_GD_Init(Port)
 {
-	if(sic_measuring_device == "TPS2014")
-	{
-		TEK_PortInit(Port);
-		TEK_Send("data:encdg srp");
-	}
-	else
-	{
-	TEK_PortInit(Port, 19200);
+	TEK_PortInit(Port);
 	TEK_Send("data:encdg rpb");
-	}
-
-	TEK_Send("data:width 1");	
+	TEK_Send("data:width 1");
 	TEK_Send("data:start 1");
 	TEK_Send("data:stop 2500");
 }
@@ -38,27 +27,15 @@ function SiC_GD_GetChannelCurve(Channel)
 	// read curve
 	var data_input = TEK_Exec("curve?");
 	//print("Channel " + Channel + " loaded");
-	
+
 	// validate data
-	if(sic_measuring_device == "TPS2014")
-	{
-		if ((data_input[0] != "#") || (data_input[1] != 4) || (data_input[2] != 5) ||
-			(data_input[3] != 0) || (data_input[4] != 0) || (data_input[5] != 0))
-		{
-			print("Invalid CH" + Channel + " data.");
-			return;
-		}
-	}
-	else
-	{
-		if ((data_input[0] != "#") || (data_input[1] != 4) || (data_input[2] != 2) ||
+	if ((data_input[0] != "#") || (data_input[1] != 4) || (data_input[2] != 2) ||
 			(data_input[3] != 5) || (data_input[4] != 0) || (data_input[5] != 0))
-		{
-			print("Invalid CH" + Channel + " data.");
-			return;
-		}
+	{
+		print("Invalid CH" + Channel + " data.");
+		return;
 	}
-	
+
 	// adjust data
 	var res = [];
 	for (var i = 6; i < 2506; ++i)
