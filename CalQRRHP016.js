a9b913f1c05a451bbb031ce573841f3f0f6f88e9
IgnatevA1 <a.ignatyev@proton-electrotex.com>
2025-03-28 13:03:03 +0300

    Добавление верификации Qrr,Irr,trr,dvdt

--- CalQRRHP016.js
+++ CalQRRHP016.js
@@ -2,20 +2,26 @@ include("Tektronix.js");
 include("TestQRRHP016.js")
 include("Sic_GetData.js")
 include("TEK_GetData.js")
+include("CaldVdt.js")
 
 // Calibration setup parameters
 cal_Rshunt = 1000;	// uOhm
 DirectCurrentTest = 1000; // in A
+DirectCurrentRateTest = 10; // in A/us
 DirectVoltageTest = 1500; // in V
 DirectVoltageRateTest = 20; // in V/us
 //
 MaxPort = 1;
 MinPort = 2;
 //
-SetCurrentTest = [320]; // in A  , 500, 800, 1000, 1200, 1500, 2000, 2500, 3000, 3200
-CurrentRateN = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-CurrentRate = [1, 1.5, 2, 5, 10, 15, 20, 30, 50, 60, 100];
-IrrMeasured = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100]; // in A
+def_UseSaveImage = true; 
+//
+SetCurrentTest = [320, 1000, 2000, 3200]; // in A  320, 500, 1000, 1500, 2000, 2500, 3000, 3200
+CurrentRateN = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+CurrentRate = [1, 1.5, 2, 5, 10, 15, 20, 30, 50, 60, 100]; // in А/us  1, 1.5, 2, 5, 10, 15, 20, 30, 50, 60, 100
+IrrMeasured = [150, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]; // in A
+SetVoltage = [402, 1000, 1800];	// in V 402, 1000, 2000, 3000, 4355
+SetVoltageRate = [20, 50, 100, 200]; // in V/us 20, 50, 100, 200
 
 CurrentRateStartTestIndex = 0;
 CurrentRateFinishTestIndex = 10;
@@ -94,6 +100,7 @@ function CAL_Init(portDevice, portTek, channelMeasureI, channelMeasureU)
 
 	// Init Tektronix port
 	TEK_PortInit(portTek);
+	TEK_GD_Init(portTek);
 	
 	// Tektronix init
 	for (var i = 1; i <= 4; i++)
@@ -131,6 +138,7 @@ function CAL_VerifyCurrent()
 		scattern(cal_dIdtSc, cal_dIdtUnitErr, "dIdt (in A/us)", "Error (in %)", "dIdt Measure error");
 	}	
 
+	//dev.w(153,0);
 	dev.c(111);
 }
 //--------------------
@@ -153,6 +161,7 @@ function CAL_VerifyTq()
 		scattern(cal_TqSc, cal_TqErr, "Tq (in us)", "Error (in %)", "Tq relative error");
 	}
 	
+	dev.w(153,0);
 	dev.c(111);
 }
 //--------------------
@@ -177,11 +186,44 @@ function CAL_VerifyQrr()
 		scattern(cal_IrrSc, cal_IrrErr, "Irr (in A)", "Error (in %)", "Irr relative error");
 		scattern(cal_TrrSc, cal_TrrErr, "Trr (in us)", "Error (in %)", "Trr relative error");
 		scattern(cal_QrrSc, cal_QrrErr, "Qrr (in uQ)", "Error (in %)", "Qrr relative error");
+		scattern(cal_IrrSc, cal_TrrErr, "Irr (in A)", "Trr Error (in %)","Trr relative error in Irr")
 	}
 	
+	dev.w(153,0);
 	dev.c(111);
 }
 //--------------------
+
+function CAL_VerifydVdt()
+{
+	CAL_ResetA();
+	
+	dev.w(153,1);
+	dev.c(110);
+
+	// Tektronix init
+	CAL_TekInitdVdt();
+	
+	if (CAL_CollectdVdt(cal_Iterations))
+	{
+		CdVdt_SaveRate("dvdt_rate", "dvdt_rate_sum");
+		CdVdt_SaveV("dvdt_v","dvdt_v_sum");
+		
+		// Plot relative error distribution
+		scattern(cdvdt_rate_sc, cdvdt_rate_err, "Voltage / Time (in V/us)", "Error relative Rate (in %)", "dVdt relative error " + SetVoltageRate.join(", ") + " V/us");
+		// scattern(cdvdt_v_sc, cdvdt_rate_err, "Voltage (in V)", "Error relative Voltage (in %)", "dVdt relative error " + SetVoltageRate.join(", ") + " V/us");
+		scattern(cdvdt_v_sc, cdvdt_v_err, "Voltage (in V)", "Error relative Voltage (in %)", "Ud relative error " + SetVoltage[0] + "..." + SetVoltage[SetVoltage.length - 1] + " V");
+	
+		scattern(cdvdt_rate_sc, cdvdt_rate_err_sum, "Voltage / Time (in V/us)", "Error relative Rate (in %)", "dVdt summary error " + SetVoltageRate.join(", ") + " V/us");
+		// scattern(cdvdt_v_sc, cdvdt_rate_err_sum, "Voltage (in V)", "Error relative Voltage (in %)", "dVdt summary error " + SetVoltageRate.join(", ") + " V/us");
+		scattern(cdvdt_v_sc, cdvdt_v_err_sum, "Voltage (in V)", "Error relative Voltage (in %)", "Ud summary error " + SetVoltage[0] + "..." + SetVoltage[SetVoltage.length - 1] + " V");
+	}
+	
+	dev.w(153,0);
+	dev.c(111);
+
+
+}
 
 
 function CAL_CollectCurrent(IterationsCount)
@@ -195,19 +237,24 @@ function CAL_CollectCurrent(IterationsCount)
 		{
 			for (var k = 0; k < SetCurrentTest.length; k++)
 			{	
-				print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
-				//
-				CAL_HorizontalScale(CurrentRateN[j]);
-				CAL_TekScale(cal_chMeasureI, SetCurrentTest[j] * cal_Rshunt / 1e6 * 2);
+				TEK_Send("horizontal:scale "  + ((SetCurrentTest[k] / CurrentRate[j]) * 1e-6) * 0.4);
+				CAL_TekScale(cal_chMeasureI, SetCurrentTest[k] * cal_Rshunt / 1e6 * 2);
 				sleep(1000);
-				qrr_print = 0;
-				//qrr_single = 1;
 
-				QRR_Start(0, SetCurrentTest[k], CurrentRateN[j], DirectVoltageTest, DirectVoltageRateTest);
+				if((SetCurrentTest[k] / CurrentRate[j]) >= 500)
+				{	
+					print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
+					break;
+				}		
+				else
+				{
+					qrr_print = 0;
+					print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
+					QRR_Start(0, SetCurrentTest[k], CurrentRateN[j], DirectVoltageTest, DirectVoltageRateTest);
+					qrr_print = 1;
+				}
 			
 				sleep(1000);
-				qrr_print = 1;
-				//qrr_single = 0;
 
 				// Set data
 			
@@ -348,85 +395,170 @@ function CAL_CollectTq(IterationsCount)
 
 function CAL_CollectQrr(IterationsCount)
 {
-	cal_CntTotal = CurrentRateN.length * IterationsCount;
+	cal_CntTotal = SetCurrentTest.length * CurrentRateN.length * IterationsCount;
 	cal_CntDone = 1;
 	
 	for (var i = 0; i < IterationsCount; i++)
 	{
 		for (var j = 0; j < CurrentRateN.length; j++)
 		{
-			print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
-			//
-			CAL_HorizontalScale(CurrentRateN[j]);
-			CAL_TekScale(cal_chMeasureI, IrrMeasured[j] * cal_Rshunt / 1e6 * 2);
-			sleep(1000);
-			
-			qrr_print = 0;
-			//qrr_single = 1;
+			for (var k = 0; k < SetCurrentTest.length; k++)
+			{
+				sleep(1000);
+				while(dev.r(192) == 5 || QSU_ReadReg(160, 192) == 5 || QSU_ReadReg(161, 192) == 5 || QSU_ReadReg(162, 192) == 5 
+					|| QSU_ReadReg(170, 192) == 5 || QSU_ReadReg(171, 192) == 5 || QSU_ReadReg(172, 192) == 5) sleep(500);
 
-			QRR_Start(0, DirectCurrentTest, CurrentRateN[j], DirectVoltageTest, DirectVoltageRateTest);
-			
-			sleep(1000);
-			qrr_print = 1;
-			//qrr_single = 0;
+				print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
+				qrr_single = 1;
+				QRR_Start(0, SetCurrentTest[k], CurrentRateN[j], DirectVoltageTest, DirectVoltageRateTest);
+				qrr_single = 0;
+				sleep(2000);
+				while(dev.r(192) == 5) sleep(500);
+				if(dev.r(198) == 1)
+				{	
+					var IrrScale = dev.r(211) / 8 / cal_Rshunt;
+					var TimeScale = dev.r(212) / 10 * 2 / 10 * 1e-6;
 
-			// Unit data
-			var Qrr = Qrr = (dev.r(219) << 16 | dev.r(216)) / 10;
-			if(QrrGOST)
-				var Qrr = (dev.r(218) << 16 | dev.r(210)) / 100;				
-			cal_Qrr.push(Qrr);
-			
-			var Irr = dev.r(211) / 10;
-			cal_Irr.push(Irr);
-			
-			var Trr = dev.r(212) / 10;
-			cal_Trr.push(Trr);
-			
+					TEK_Horizontal(TimeScale, "0");
+					TEK_HorizontalPosition(5);
+					CAL_TekScale(cal_chMeasureI, IrrScale); 
+					sleep(2000);
+					QRR_Start(0, SetCurrentTest[k], CurrentRateN[j], DirectVoltageTest, DirectVoltageRateTest);
+				}	
 
-			// Scope data
-			var ScopeData = CAL_MeasureQrr(cal_chMeasureI);
-			var IrrSc = parseFloat(ScopeData[0]).toFixed(2);
-			var TrrSc = parseFloat(ScopeData[1]).toFixed(2);
-			var QrrSc = parseFloat(ScopeData[2]).toFixed(2);
-			if(QrrGOST)
-				QrrSc = parseFloat(ScopeData[3]).toFixed(2);			
-			cal_IrrSc.push(IrrSc);
-			cal_TrrSc.push(TrrSc);
-			cal_QrrSc.push(QrrSc);
+				sleep(1000);
+
+				// Unit data
+				if(dev.r(196) == 0)
+				{	
+					var Qrr = Qrr = (dev.r(219) << 16 | dev.r(216)) / 10;
+					if(QrrGOST)
+						var Qrr = (dev.r(218) << 16 | dev.r(210)) / 100;				
+					cal_Qrr.push(Qrr);
 			
+					var Irr = dev.r(211) / 10;
+					cal_Irr.push(Irr);
 			
-			// Relative error
-			var IrrErr = ((Irr - IrrSc) / IrrSc * 100).toFixed(2);
-			var TrrErr = ((Trr - TrrSc) / TrrSc * 100).toFixed(2);
-			var QrrErr = ((Qrr - QrrSc) / QrrSc * 100).toFixed(2);
+					var Trr = dev.r(212) / 10;
+					cal_Trr.push(Trr);
+
+					// Scope data
+					var ScopeData = CAL_MeasureQrr(cal_chMeasureI);
+					var IrrSc = parseFloat(ScopeData[0]).toFixed(2);
+					var TrrSc = parseFloat(ScopeData[1]).toFixed(2);
+					var QrrSc = parseFloat(ScopeData[2]).toFixed(2);
+					if(QrrGOST)
+						QrrSc = parseFloat(ScopeData[3]).toFixed(2);			
+					cal_IrrSc.push(IrrSc);
+					cal_TrrSc.push(TrrSc);
+					cal_QrrSc.push(QrrSc);
 			
-			cal_IrrErr.push(IrrErr);
-			cal_TrrErr.push(TrrErr);
-			cal_QrrErr.push(QrrErr);
+					// Relative error
+					var IrrErr = ((Irr - IrrSc) / IrrSc * 100).toFixed(2);
+					var TrrErr = ((Trr - TrrSc) / TrrSc * 100).toFixed(2);
+					var QrrErr = ((Qrr - QrrSc) / QrrSc * 100).toFixed(2);
+		
+					cal_IrrErr.push(IrrErr);
+					cal_TrrErr.push(TrrErr);
+					cal_QrrErr.push(QrrErr);
 			
-			
-			
-			
-			// Print results
-			print("");
-			print("Irr, A	 : " + Irr);
-			print("IrrTek,  A: " + IrrSc);
-			print("IrrErr,  %: " + IrrErr);
-			print("");
-			print("Trr, us	 : " + Trr);
-			print("TrrTek, us: " + TrrSc);
-			print("TrrErr,  %: " + TrrErr);
-			print("");
-			print("Qrr, uQ	 : " + Qrr);
-			print("QrrTek, uQ: " + QrrSc);
-			print("QrrErr,  %: " + QrrErr);
-			print("--------------------");
-			
-			if (anykey()) return 0;
+					ChannelDataPlot(cal_chMeasureI, SetCurrentTest[k] + "A " + CurrentRate[j] + " A/us");
+
+					// Print results
+					print(SetCurrentTest[k] + " A" + " , " + CurrentRate[j] + "A/us ")
+					print("");
+					print("Irr, A	 : " + Irr);
+					print("IrrTek,  A: " + IrrSc);
+					print("IrrErr,  %: " + IrrErr);
+					print("");
+					print("Trr, us	 : " + Trr);
+					print("TrrTek, us: " + TrrSc);
+					print("TrrErr,  %: " + TrrErr);
+					print("");
+					print("Qrr, uQ	 : " + Qrr);
+					print("QrrTek, uQ: " + QrrSc);
+					print("QrrErr,  %: " + QrrErr);
+					print("--------------------");
+				}	
+			// if (anykey()) return 0;	
+			}				
 		}
 	}
 
 	return 1;
+}
+//--------------------
+
+function CAL_CollectdVdt(IterationsCount)
+{
+	CdVdt_ResetA();
+
+	print("      dV/dt, V/us      |       Voltage, V      ");
+	print("  set  |  osc  |  err  |  set  |  osc  |  err  ");
+	print("-----------------------------------------------");
+
+	for (var i = 0; i < IterationsCount; i++)
+	{
+		for (var j = 0; j < SetVoltageRate.length; j++)
+		{
+			for (var k = 0; k < SetVoltage.length; k++)
+			{
+				CdVdt_TekVScale(cal_chMeasureU, SetVoltage[k]);
+				CdVdt_TekHScale(cal_chMeasureU, SetVoltage[k], SetVoltageRate[j]);
+				TEK_TriggerInit(cal_chMeasureU, SetVoltage[k] / 2);
+				CdVdt_ClearDisplay();
+				TEK_Busy();
+				qrr_single = 1;
+				QRR_Start(1, DirectCurrentTest, DirectCurrentRateTest, SetVoltage[k], SetVoltageRate[j]);
+				qrr_single = 0;
+				sleep(3000);
+				TEK_Busy();
+				var v = CdVdt_MeasureVfast();
+				TEK_Busy();
+				var rate = SiC_CALC_dVdt(SiC_GD_GetChannelCurve(cal_chMeasureU),10,90).toFixed(1);
+				TEK_Busy();
+				dVdt_err = (rate - SetVoltageRate[j]) / SetVoltageRate[j] * 100;
+				dVdt_err = Math.abs(dVdt_err) < 0.1 ? parseFloat(0).toFixed(1) : dVdt_err.toFixed(1);
+				V_err = (v - SetVoltage[k]) / SetVoltage[k] * 100;
+				V_err = Math.abs(V_err) < 0.1 ? parseFloat(0).toFixed(1) : V_err.toFixed(1);
+
+				var ETosc = 0;
+
+				cdvdt_rate_set.push(SetVoltageRate[j]);
+				cdvdt_v_set.push(SetVoltage[k]);
+
+				cdvdt_rate_sc.push(rate);
+				cdvdt_v_sc.push(v);
+
+				cdvdt_rate_err.push(dVdt_err);
+				cdvdt_v_err.push(V_err);
+
+				// Summary error
+				E0dvdt = 1.1 * Math.sqrt(Math.pow(EUosc, 2) + Math.pow(ETosc, 2) + Math.pow(EProbe, 2));
+				dVdt_err_sum = (CdVdt_sign(dVdt_err)*(Math.abs(dVdt_err) + E0dvdt)).toFixed(1)
+				cdvdt_rate_err_sum.push(dVdt_err_sum);
+
+				E0V = 1.1 * Math.sqrt(Math.pow(EUosc, 2) + Math.pow(EProbe, 2));
+				V_err_sum = (CdVdt_sign(V_err)*(Math.abs(V_err) + E0V)).toFixed(1)
+				cdvdt_v_err_sum.push(V_err_sum);
+
+				print("  " + SetVoltageRate[j] + (SetVoltageRate[j] < 100 ? " " : "") + (SetVoltageRate[j] < 1000 ? " " : "") + " | " + rate + (rate < 100 ? " " : "") + (rate < 1000 ? " " : "") + "| " + (dVdt_err >= 0 ? " " : "") + dVdt_err + (Math.abs(dVdt_err) < 10 ? " " : "") + " |  " + SetVoltage[k] + (SetVoltage[k] < 100 ? " " : "") + (SetVoltage[k] < 1000 ? " " : "") + " | " + v + (v < 100 ? " " : "") + (v < 1000 ? " " : "") + "  | " + (V_err >= 0 ? " " : "") + V_err);
+
+				if (def_UseSaveImage)
+				{
+					var NameFile = "" + SetVoltage[k] + SetVoltageRate[j] + "";
+					var SaveImage = "save:image \"A:\\" + NameFile + ".BMP\"";
+					TEK_Send(SaveImage);
+					sleep(3000);
+					TEK_Busy();
+				}
+				// if (anykey()){ print("Stopped from user!"); return};
+			}
+
+		}	
+	}
+
+	return 1;		
 }
 //--------------------
 
@@ -648,15 +780,16 @@ function CAL_MeasureCurrent(Channel)
 function CAL_MeasureQrr(Channel)
 {
 	var CurrentScale = 0, Current = 0, IntegratedCurrent = 0;
-	var Index0 = 0, Index09 = 0, Index025 = 0, IndexIrr = 0, IndexTrr = 0;
+	var Index0 = 0, Index09 = 0, Index050 = 0, Index025 = 0, IndexIrr = 0, IndexTrr = 0;
 	var k = 0, b = 0, TimeFraction;
 	var Result = [];
 	var ResultIrr, ResultTrr, ResultQrr, ResultQrrGOST;
 	
 	// Get waveform
 	CurrentScale = 1 / cal_Rshunt * 1e6;
-	Current = SiC_GD_Filter(SiC_GD_GetChannelCurve(Channel), CurrentScale);
 	
+	Current = (TEK_GD_Filter(SiC_GD_GetChannelCurve(Channel), CurrentScale));
+
 	TimeFraction = SiC_GD_GetTimeScale() / 250 * 1e9  / 1000;
 	
 	// Searching zero crossing point
@@ -675,7 +808,19 @@ function CAL_MeasureQrr(Channel)
 		if(Current[i] < Current[IndexIrr])
 			IndexIrr = i;
 	}
+	// Searching Irr * 0.5 point
+	// for(i = IndexIrr; i < Current.length; i++)
+	// {
+		// if(Current[i] > Current[IndexIrr] * 0.5)
+		// {
+			// Index05 = i;
+			// break;
+		// }
+	// }
+	// SiC_Approx2(Current, Index05);
 	
+
+
 	// Searching Irr * 0.9 point
 	for(i = IndexIrr; i < Current.length; i++)
 	{
@@ -685,7 +830,6 @@ function CAL_MeasureQrr(Channel)
 			break;
 		}
 	}
-	
 	// Searching Irr * 0.25 point
 	for(i = IndexIrr; i < Current.length; i++)
 	{
@@ -836,15 +980,15 @@ function CAL_TekInitCurrent()
 
 function CAL_TekInitQrr()
 {
-	TEK_Horizontal("1e-6", "0");
+	TEK_Horizontal("1e-4", "0");
 	
 	TEK_ChannelInvInit(cal_chMeasureI, "1", "0.1");
-	TEK_Send("ch" + cal_chMeasureI + ":position 0");
+	TEK_Send("ch" + cal_chMeasureI + ":position 2");
 	
 	TEK_ChannelInit(cal_chMeasureU, "100", "20");
 	TEK_Send("ch" + cal_chMeasureU + ":position 3");
 	
-	TEK_TriggerInit(cal_chMeasureI, "-0.09");
+	TEK_TriggerInit(cal_chMeasureI, "-0.05");
 	TEK_Send("trigger:main:edge:slope rise");
 	
 	TEK_AcquireAvg(4);
@@ -871,6 +1015,24 @@ function CAL_TekInitTq()
 }
 //--------------------
 
+function CAL_TekInitdVdt()
+{
+	TEK_ChannelOff(cal_chMeasureI);
+	TEK_ChannelOn(cal_chMeasureU);
+
+	CdVdt_TekMeasurement(cal_chMeasureU);
+
+	TEK_Horizontal("10e-6", "0");
+	
+	TEK_ChannelInit(cal_chMeasureU, cdvdt_HVProbeScale, "100");
+	TEK_Send("ch" + cal_chMeasureU + ":position -4");
+	
+	TEK_TriggerInit(cal_chMeasureU, "100");
+	TEK_Send("trigger:main:edge:slope raise");
+
+}
+//--------------------
+
 function CAL_TekScale(Channel, Value)
 {
 	Value = Value / 6;
@@ -883,48 +1045,48 @@ function CAL_HorizontalScale(CurrentRateN)
 	switch(CurrentRateN)
 	{
 		case 0:
-		//	TEK_Horizontal("100e-6", "0");
 			TEK_Horizontal("100e-6", "0");
+			
 			break;
 		case 1:
-		//	TEK_Horizontal("50e-6", "0.2e-3");
-			TEK_Horizontal("100e-6", "0");
+			TEK_Horizontal("50e-6", "0.2e-3");
+			
 			break;
 		case 2:
-		//	TEK_Horizontal("50e-6", "0.2e-3");
-			TEK_Horizontal("50e-6", "5e-5");
+			TEK_Horizontal("50e-6", "0.2e-3");
+			
 			break;
 		case 3:
-		//	TEK_Horizontal("50e-6", "0.2e-3");
-			TEK_Horizontal("25e-6", "0");
+			TEK_Horizontal("50e-6", "0.2e-3");
+			
 			break;
 		case 4:
-		//	TEK_Horizontal("25e-6", "0.1e-3");
-			TEK_Horizontal("10e-6", "0");
+			TEK_Horizontal("25e-6", "0.1e-3");
+			
 			break;
 		case 5:
-		//	TEK_Horizontal("10e-6", "3e-5");
-			TEK_Horizontal("10e-6", "0");
+			TEK_Horizontal("10e-6", "3e-5");
+			
 			break;
 		case 6:
-		//	TEK_Horizontal("10e-6", "3e-5");
-			TEK_Horizontal("5e-6", "0");
+			TEK_Horizontal("10e-6", "3e-5");
+			
 			break;
 		case 7:
-		//	TEK_Horizontal("10e-6", "1.5e-5");
-			TEK_Horizontal("5e-6", "0");
+			TEK_Horizontal("10e-6", "1.5e-5");
+			
 			break;
 		case 8:
-		//	TEK_Horizontal("10e-6", "1.5e-5");
-			TEK_Horizontal("2.5e-6", "0");
+			TEK_Horizontal("10e-6", "1.5e-5");
+			
 			break;
 		case 9:
-		//	TEK_Horizontal("8e-6", "5.5e-6");
-			TEK_Horizontal("2.5e-6", "0");
+			TEK_Horizontal("8e-6", "5.5e-6");
+			
 			break;
 		case 10:
-		//	TEK_Horizontal("5e-6", "6.5e-6");
-			TEK_Horizontal("1e-6", "1e-6");
+			TEK_Horizontal("5e-6", "6.5e-6");
+
 			break;
 	}
 }
