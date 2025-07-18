8056008c94d8568aed8ac7d4d040982a65bb1b6a
floeenica <a250kj@gmail.com>
2025-07-18 19:09:34 +0300

    изменено под работу с DMM6500

--- CalLCSU.js
+++ CalLCSU.js
@@ -1,454 +1,138 @@
-include("TestLCSU.js")
-include("Tektronix.js")
 include("CalGeneral.js")
+include("6500.js")
+include("TestLCSU.js")
+
+PulseType = 0; // 0 - синус, 1 - модифицированный синус, 2 - трапеция
 
 // Calibration setup parameters
-cal_Points = 13;
+clcsu_Rshunt = 250;	// мкОм
 
-cal_Rshunt = 999;	// uOhm
-
-cal_CurrentRange = 1;
-
-cal_IdMin = [50, 301];	
-cal_IdMax = [300, 1700];
-cal_IdStp = (cal_IdMax[cal_CurrentRange] - cal_IdMin[cal_CurrentRange]) / cal_Points;
-
-cal_Iterations = 1;
-cal_UseAvg = 1;
-//		
-
-// Counters
-cal_CntTotal = 0;
-cal_CntDone = 0;
-
-// Channels
-cal_chMeasureId = 1;
-cal_chSync = 3;
-
-// Results storage
-cal_Id = [];
-cal_IdMes = [];
-// Tektronix data
-cal_IdSc = [];
-
-// Relative error
-cal_IdErr = [];
-cal_IdErrMes = [];
+// Обозначение диапазонов калибровки
+clcsu_CurrentRange = 0; // 0 = диапазон [ <= 350 A]; 1 = диапазон [ < 1100 A]; 2 = диапазон [ < 6500 A]
+var LCSU_Range = new Array(1); // массив номеров регистров К и B для разных диапазонов
+CLCSU_Range(clcsu_CurrentRange); 
 //
+clcsu_Points = 3; // кол-во точек калибровки (токов) внутри диапазона калибровки
+//
+clcsu_IdMin = [160, 351, 1101];
+clcsu_IdMax = [699, 1099, 6500]; // должно быть [349, 1099, 6500] в новой прошивке, значение 699 взято для тестов на 2-диапазонном блоке!
+clcsu_IdStp = (clcsu_IdMax[clcsu_CurrentRange] - clcsu_IdMin[clcsu_CurrentRange]) / clcsu_Points;
+//
+clcsu_Iterations = 5; // количество итераций накопления статистики
+clcsu_SaveImage = 0;
 
-// Correction
-cal_IdCorr = [];
-
-function CAL_Init(portDevice, portTek, channelMeasureId)
-{
-	if (channelMeasureId < 1 || channelMeasureId > 4)
-	{
-		print("Wrong channel numbers");
-		return;
-	}
-
-	// Copy channel information
-	cal_chMeasureId = channelMeasureId;
-
-	// Init device port
-	dev.Disconnect();
-	dev.Connect(portDevice);
-
-	// Init Tektronix port
-	TEK_PortInit(portTek);
-	
-	// Tektronix init
-	for (var i = 1; i <= 4; i++)
-	{
-		if (i == channelMeasureId)
-			TEK_ChannelOn(i);
-		else
-			TEK_ChannelOff(i);
-	}
-}
-//--------------------
-
-function CAL_VerifyId()
+function CLCSU_VerifyId()
 {		
-	CAL_ResetA();
-	
-	// Tektronix init
-	CAL_TekInit();
 
 	// Reload values
-	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
+	var CurrentArray = CGEN_GetRangeLogarithm(clcsu_IdMin[clcsu_CurrentRange], clcsu_IdMax[clcsu_CurrentRange], clcsu_Points);
 
-	if (CAL_CollectId(CurrentArray, cal_Iterations))
+	if (CLCSU_CollectId(CurrentArray, clcsu_Iterations))
 	{
-		CAL_SaveId("LCSU_Id_fixed");
+		CLSLPC_SaveId("LCSU_Id_fixed");
 
 		// Plot relative error distribution
-		scattern(cal_IdSc, cal_IdErr, "Current (in A)", "Error (in %)", "Current setpoint relative error");
+		scattern(clcsu_IdSc, clcsu_IdErr, "Current (in A)", "Error (in %)", "Current setpoint relative error "
+				+ clcsu_IdMin[clslpc_CurrentRange] + " A ... " + clcsu_IdMax[clslpc_CurrentRange] + " A");
+		scattern(clcsu_IdSc, clcsu_IdUnitErr, "Current (in A)", "Error (in %)", "Current unit relative error "
+				+ clcsu_IdMin[clcsu_CurrentRange] + " A ... " + clcsu_IdMax[clslpc_CurrentRange] + " A");
 	}
 }
-//--------------------
-function CAL_CalibrateId()
-{		
-	CAL_ResetA();
-	CAL_ResetIdCal();
-	
-	// Tektronix init
-	CAL_TekInit(cal_chMeasureId);
 
-	// Reload values
-	var cal_IdStp = Math.round((cal_IdMax[cal_CurrentRange] - cal_IdMin[cal_CurrentRange]) / (cal_Points - 1));
-	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
-
-	if (CAL_CollectId(CurrentArray, cal_Iterations))
-	{
-		CAL_SaveId("LSLPC_Id");
-
-		// Plot relative error distribution
-		scattern(cal_IdSc, cal_IdErr, "Current (in A)", "Error (in %)", "Current setpoint relative error");
-
-		// Calculate correction
-		cal_IdCorr = CGEN_GetCorrection2("LSLPC_Id");
-		CAL_SetCoefId(cal_IdCorr[0], cal_IdCorr[1], cal_IdCorr[2]);
-		CAL_PrintCoefId();
-	}
-}
-//-------------------------
-
-function CAL_ResetA()
-{	
-	// Results storage
-	cal_Id = [];
-
-	// Tektronix data
-	cal_IdSc = [];
-
-	// Relative error
-	cal_IdErr = [];
-
-	// Correction
-	cal_IdCorr = [];
-}
-//--------------------
-function CAL_TekInit()
+function CLCSU_CollectId(CurrentArray, clcsu_Iterations, PulseType)
 {
-	TEK_ChannelInit(cal_chMeasureId, "1", "0.01");
-	TEK_TriggerPulseInit(cal_chMeasureId, "0.04");
-	TEK_Horizontal("0.250e-3", "0");
-	TEK_Send("measurement:meas" + cal_chMeasureId + ":source ch" + cal_chMeasureId);
-	TEK_Send("measurement:meas" + cal_chMeasureId + ":type maximum");
-}
-//--------------------
+	clcsu_CntTotal = clcsu_Iterations * CurrentArray.length;
+	//clcsu_CntDone = 1;
+	print("Кол-во итераций: " + clcsu_CntTotal);
 
-function CAL_CollectId(CurrentValues, IterationsCount)
-{
-	cal_CntTotal = IterationsCount * CurrentValues.length;
-	cal_CntDone = 1;
+	var CurrentValues = new Array(clcsu_CntTotal); // массив значений измеренных токов
+	var DACValues = new Array(clcsu_CntTotal); // массив значений ЦАП
+	var deltaCurrents = new Array(clcsu_CntTotal); // массив отклонений измеренных LCSU CtrlBrd значений от измеренных 6500
+	var CurrentArray_repeat = new Array(clcsu_CntTotal); // массив задаваемых токов, повторяемый clcsu_Iterations раз, используется для вывода графика погрешности
+	var sum_Current_square = 0, sum_Current  = 0, sum_DAC = 0, sum_Common = 0; // коэффициенты для расчета аппроксимационной прямой
 
-	var AvgNum;
-	if (cal_UseAvg)
+	for (var i = 0; i < clcsu_Iterations; i++)
 	{
-		AvgNum = 4;
-		TEK_AcquireAvg(AvgNum);
-	}
-	else
-	{
-		AvgNum = 1;
-		TEK_AcquireSample();
-	}
-	
-	for (var i = 0; i < IterationsCount; i++)
-	{
-		for (var j = 0; j < CurrentValues.length; j++)
+		for (var j = 0; j < CurrentArray.length; j++)
 		{
-			print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
-			//
-			LCSU_TekScale(cal_chMeasureId, CurrentValues[j] * cal_Rshunt / 1000000);
-			
-			for (var k = 0; k < AvgNum; k++)
-			{
-				if(!LCSU_Start(CurrentValues[j]))
-					return false;
-			}
-			
-			// Unit data
-			var IdSet = dev.r(128);
-			cal_Id.push(IdSet);
-			print("Idset, A: " + IdSet);
+			KEI_Wait();
+			sleep(1500);
+			LCSU_Start(PulseType, CurrentArray[j]);
+			sleep(200);
 
-			// Scope data
-			var IdSc = (CAL_Measure(cal_chMeasureId) / cal_Rshunt * 1000000).toFixed(2);
-			cal_IdSc.push(IdSc);
-			print("Idtek, A: " + IdSc);
+			Id_DACArray = dev.raff(6);
+			var IdDAC = Math.max.apply(null, Id_DACArray);
 
-			// Relative error
-			var IdErr = ((IdSet - IdSc) / IdSc * 100).toFixed(2);
-			cal_IdErr.push(IdErr);
-			print("IdSetErr, %: " + IdErr);
+			var IdUnit = dev.rf(200); // Ток измеренный LCSU CtrlBrd
+			var IdSc = KEI_Current(); // Ток измеренный DMM6500, он же realCurrent в старом скрипте
+			var IdSet = dev.rf(128); // Задаваемый ток, он же Current_actual в старом скрипте
+			var IdErr = ((IdSet - IdSc) / IdSc * 100).toFixed(2); // погрешность заданного тока относительно измеренного dmm6500
+			//clcsu_Id.push(IdSet);
+			//clcsu_IdSc.push(IdSc);
+			//clcsu_IdErr.push(IdErr);
+			sum_Current_square += (IdSc*IdSc);
+			sum_Current +=IdSc;
+			sum_DAC += IdDAC;
+			sum_Common += IdSc*IdDAC;
+
+			CurrentValues[(i*CurrentValues.length)+j] = IdSc;
+			DACValues[(i*CurrentValues.length)+j] = IdDAC;
+			deltaCurrents[(i*CurrentValues.length)+j] = IdErr;
+			CurrentArray_repeat[(i*CurrentValues.length)+j] = CurrentArray[j];
+
+			print("Значение выхода ЦАП: " + IdDAC);
+			print("Заданный ток, A: " + IdSet);
+			print("Измеренный мультиметром ток, A: " + IdSc);
+			print("Измеренный LCSU CtrlBrd ток, A: " + IdUnit);
+			print("Погрешность LCSU CtrlBrd относительно DMM6500, %: " + IdErr);
 			print("--------------------");
-
-
-			
-			if (anykey()) return 0;
 		}
 	}
-
-	return 1;
+	scattern(CurrentArray_repeat, deltaCurrents, "Заданный ток, А", "Отклонение, %", "Отклонение измеренного тока от заданного, 160...700 А");
+	CLCSU_CalibrateDAC(sum_Current_square, sum_Current, sum_DAC, sum_Common, clcsu_CntTotal, clcsu_CntTotal);
 }
-//--------------------
-function LCSU_TekScale(Channel, Value)
+
+function CLCSU_CalibrateDAC(sum_Current_square, sum_Current, sum_DAC, sum_Common, clcsu_CntTotal, clcsu_CntTotal)
 {
-	Value = Value / 6;
-	TEK_Send("ch" + Channel + ":scale " + Value);
-	
-	TEK_TriggerPulseInit(cal_chMeasureId, Value * 1);
+	var coeff_n1 = sum_Current_square*sum_DAC/sum_Current;
+	var coeff_b1 = sum_Current_square*(-(clcsu_CntTotal))/sum_Current;
+	var coeff_b2 = coeff_b1+sum_Current;
+	var coeff_n2 = sum_Common - coeff_n1;
+	var CLCSU_B = coeff_n2/coeff_b2;
+	var CLCSU_K = (sum_DAC-(clcsu_CntTotal*CLCSU_B))/sum_Current;
+	p("Коэффициент К: " + CLCSU_K);
+	p("Коэффициент B: " + CLCSU_B);
+	sum_Current_square = 0, sum_Current  = 0, sum_DAC = 0, sum_Common = 0;
+
+	if (CLCSU_K < 0.01 || CLCSU_K > 5 || CLCSU_B<500 || CLCSU_B>2000)
+		{
+			p("Коэффициенты абсолютно точно неправильны. Пожалуйста, проведите расчет снова. Записаны дефолтные коэффициенты");
+			dev.wf(LCSU_Range[0],2.2);
+			dev.wf(LCSU_Range[1],1000);	
+		}
+		else 
+		{
+			p("Коэффициенты записаны. Не забывайте про команду dev.c(200) для записи в энергонезависимую память");
+			dev.wf(LCSU_Range[0],CLCSU_K);
+			dev.wf(LCSU_Range[1],CLCSU_B);	
+		}
 }
-//--------------------
 
-function CAL_Measure(Channel)
+function CLCSU_Range(clcsu_CurrentRange) // Функция для определения номера регистров записи коэффициентов
 {
-	return TEK_Measure(Channel);
-}
-//--------------------
-
-function CAL_SaveId(NameId)
-{
-	CGEN_SaveArrays(NameId, cal_Id, cal_IdSc, cal_IdErr);
-}
-//--------------------
-
-function CAL_CollectMesure(CurrentValues, IterationsCount)
-{
-	cal_CntTotal = IterationsCount * CurrentValues.length;
-	cal_CntDone = 1;
-
-	var AvgNum;
-	if (cal_UseAvg)
+	if (clcsu_CurrentRange === 0) // 160...350 A
 	{
-		AvgNum = 4;
-		TEK_AcquireAvg(AvgNum);
+		LCSU_Range[0] = 23; // номера регистров
+		LCSU_Range[1] = 24;
 	}
-	else
-	{
-		AvgNum = 1;
-		TEK_AcquireSample();
-	}
-	
-	for (var i = 0; i < IterationsCount; i++)
-	{
-		for (var j = 0; j < CurrentValues.length; j++)
+	if (clcsu_CurrentRange = 1) //350...1100 A
 		{
-			print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
-			//
-			LCSU_TekScale(cal_chMeasureId, CurrentValues[j] * cal_Rshunt / 1000000);
-			
-			for (var k = 0; k < AvgNum; k++)
-			{
-				if(!LCSU_Start(CurrentValues[j]))
-					return false;
-			}
-			
-			// Unit data
-			var IdSet = dev.rf(128);
-			cal_Id.push(IdSet);
-			print("Idset, A: " + IdSet);
-
-			// Unit data
-			var IdMes = dev.rf(200);
-			cal_IdMes.push(IdMes);
-			print("IdMes, A: " + IdMes);
-
-			// Scope data
-			var IdSc = (CAL_Measure(cal_chMeasureId) / cal_Rshunt * 1000000).toFixed(3);
-			cal_IdSc.push(IdSc);
-			print("Idtek, A: " + IdSc);
-
-			// Relative error
-			var IdErr = ((IdSet - IdSc) / IdSc * 100).toFixed(2);
-			cal_IdErr.push(IdErr);
-			print("IdSetErr, %: " + IdErr);
-
-			var IdErrMes = ((IdMes - IdSc) / IdSc * 100).toFixed(2);
-			cal_IdErrMes.push(IdErrMes);
-			print("IdMesErr, %: " + IdErrMes);
-			print("--------------------");
-
-
-			
-			if (anykey()) return 0;
+			LCSU_Range[0] = 28;
+			LCSU_Range[1] = 29;
 		}
-	}
-
-	return 1;
-}
-
-function CAL_VerifyMesure()
-{		
-	CAL_ResetA();
-	
-	// Tektronix init
-	CAL_TekInit();
-
-	// Reload values
-	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
-
-	if (CAL_CollectMesure(CurrentArray, cal_Iterations))
-	{
-		CAL_SaveId("LCSU_Id_fixed");
-
-		// Plot relative error distribution
-		scattern(cal_IdSc, cal_IdErrMes, "Current (in A)", "Error (in %)", "Current setpoint relative error");
-	}
-}
-//--------------------
-function CAL_CalibrateMesure()
-{		
-	CAL_ResetA();
-	CAL_ResetIdCalMes()
-	// Tektronix init
-	CAL_TekInit();
-
-	// Reload values
-	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
-
-	if (CAL_CollectMesure(CurrentArray, cal_Iterations))
-	{
-		CAL_SaveId("LCSU_Id_fixed");
-
-		// Plot relative error distribution
-		scattern(cal_IdSc, cal_IdErr, "Current (in A)", "Error (in %)", "Current setpoint relative error");
-
-		cal_IdCorr = CGEN_GetCorrection2("LCSU_Id_fixed");
-		CAL_SetCoefIdMes(cal_IdCorr[0], cal_IdCorr[1], cal_IdCorr[2]);
-		CAL_PrintCoefIdMes();
-	}
-}
-//--------------------
-
-function CAL_ResetIdCal()
-{
-	CAL_SetCoefId(0, 1, 0);
-}
-//--------------------
-function CAL_SetCoefId(P2, P1, P0)
-{
-	switch(cal_CurrentRange)
-	{	
-		case 0:
+	if (clcsu_CurrentRange === 2) // 1100... 6500 A
 		{
-			dev.wf(20, P2);
-			dev.wf(21, P1);
-			dev.wf(22, P0);
-		}
-		break;
-		
-		case 1:
-		{
-			dev.wf(25, P2);
-			dev.wf(26, P1);
-			dev.wf(27, P0);
-		}
-		break;
-	}		
-}
-//--------------------
-function CAL_PrintCoefId()
-{
-	switch(cal_CurrentRange)
-	{
-		case 0:
-		{
-			print("Id 0 P2	: " + dev.rf(20));
-			print("Id 0 P1	: " + dev.rf(21));
-			print("Id 0 P0	: " + dev.rf(22));
-		}
-		break;
-		
-		case 1:
-		{
-			print("Id 1 P2	: " + dev.rf(25));
-			print("Id 1 P1	: " + dev.rf(26));
-			print("Id 1 P0	: " + dev.rf(27));
-		}
-		break
-	}
-}
-//--------------------
-function CAL_SetCoefIdMes(P2, P1, P0)
-{
-	switch(cal_CurrentRange)
-	{	
-		case 0:
-		{
-			dev.wf(34, P2);
-			dev.wf(35, P1);
-			dev.wf(36, P0);
-		}
-		break;
-		
-		case 1:
-		{
-			dev.wf(39, P2);
-			dev.wf(40, P1);
-			dev.wf(41, P0);
-		}
-		break;
-	}		
-}
-//--------------------
-
-function CAL_ResetIdCalMes()
-{
-	CAL_SetCoefIdMes(0, 1, 0);
-}
-//--------------------
-function CAL_PrintCoefIdMes()
-{
-	switch(cal_CurrentRange)
-	{
-		case 0:
-		{
-			print("Id 0 P2	: " + dev.rf(34));
-			print("Id 0 P1	: " + dev.rf(35));
-			print("Id 0 P0	: " + dev.rf(36));
-		}
-		break;
-		
-		case 1:
-		{
-			print("Id 1 P2	: " + dev.rf(39));
-			print("Id 1 P1	: " + dev.rf(40));
-			print("Id 1 P0	: " + dev.rf(41));
-		}
-		break
-	}
-}
-//--------------------
-RShunt = 0.00025; // Сопротивление шунта, Ом
-
-function KEI_Wait(Current_abt) // Функция для перевода мультиметра DMM6500 в режим измерения тока LCSU
-{
-	BufferLength = 150; // длительность записи (15 мс, 150 точек)
-	Sync_level = Current_abt*RShunt*0.2; // уровень синхронизации, 20% от ном. уровня
-	tmc.w(':TRACe:CLEar "TestBuffer"');
-
-	tmc.w(':SENSe:DIGitize:FUNCtion "VOLTage"');
-	tmc.w(':SENSe:DIGitize:VOLTage:SRATe 10000'); // fs (10 кГц)
-	tmc.w(':DIGitize:VOLTage:ATRigger:MODE EDGE');
-	tmc.w(':DIGitize:VOLTage:ATRigger:EDGE:LEVel' + Sync_level);
-	tmc.w(':DIGitize:VOLTage:ATRigger:EDGE:SLOPe RISing');
-
-	tmc.w('TRACe:MAKE "TestBuffer",' + BufferLength);
-	tmc.w('TRACe:FILL:MODE CONTinuous, "TestBuffer"');
-	tmc.w('TRACe:LOG:STATe ON , "TestBuffer"');
-
-	tmc.w(':TRIGger:LOAD "LoopUntilEvent", ATRigger, 10, ENTer, 0, "TestBuffer"');
-	tmc.w(':INITiate');
-
-	tmc.w(':DISPlay:BUFFer:ACTive "TestBuffer"');
-	tmc.w(':DISPlay:SCReen GRAPh');
-}
-//--------------------
-function KEI_Current()
-{
-	p("Ток, А " + 4000*tmc.q(':TRACe:STAT:MAXimum? "TestBuffer"'));
+			LCSU_Range[0] = 67;
+			LCSU_Range[1] = 68;
+		}	
 }
