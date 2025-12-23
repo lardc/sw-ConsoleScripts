1d6e7b468ac79cf1970c168cdf7b30edf71b2377
asytyi <alexander.a.sytyi@gmail.com>
2025-12-23 12:48:51 +0300

    Добавлена проверка завершения прошивки + флаги управления параметрами мастера

--- FirmwareCommon.js
+++ FirmwareCommon.js
@@ -2,8 +2,8 @@ function Firmware_Get(NodeID)
 {
 	dev.nid(NodeID)
 	var StrLen = dev.r(260)
-	
-	if(StrLen > 0)
+
+	if (StrLen > 0)
 	{
 		var Str = ""
 		for (var i = 0; i < StrLen / 2; i++)
@@ -12,66 +12,66 @@ function Firmware_Get(NodeID)
 			Str = Str.concat(String.fromCharCode(Word >> 8))
 			Str = Str.concat(String.fromCharCode(Word & 0xFF))
 		}
-		
+
 		// Удаление пробельных символов в конце строки
 		Str = Str.replace(/\s+$/g, '')
-		
+
 		var str_arr = Str.split(",")
-		
+
 		// branch, commit, commitTimestamp, project
 		return [str_arr[2], str_arr[0], str_arr[1], str_arr[3]]
 	}
 }
 
-function Firmware_Update(NodeID, FileName)
+function Firmware_Update(NodeID, FileName, CheckMaster, CheckDate)
 {
 	// Базовая проверка имени файла
 	var InputData = FileName.replace(/\.[a-z]+$/, "").split("_")
-	if(!InputData || InputData.length < 3)
+	if (!InputData || InputData.length < 3)
 	{
 		print("Filename with incorrect format")
 		return
 	}
-	
+
 	var FileProject = InputData[0]
 	var FileDate = new Date(InputData[1].replace(/\./g, "/") + "T" + InputData[2].replace(/\./g, ":") + ":00")
-	
+
 	// Получение и базовая проверка данных ЭМ
 	var BoardData = Firmware_Get(NodeID)
-	if(!BoardData || BoardData.length < 4)
+	if (!BoardData || BoardData.length < 4)
 	{
 		print("Board firmware information incomplete")
 		return
 	}
-	
+
 	var BoardBranch = BoardData[0]
 	var BoardProject = BoardData[3]
 	var BoardDate = new Date(BoardData[2])
-	
+
 	// Проверка условий перепрошивки
-	if(BoardBranch !== "master")
+	if (CheckMaster && (BoardBranch !== "master"))
 	{
 		print("Not master branch on board")
 		return
 	}
-	
-	if(BoardProject !== FileProject)
+
+	if (BoardProject !== FileProject)
 	{
 		print("Project name missmatch")
 		return
 	}
-	
-	if(BoardDate >= FileDate)
+
+	if (CheckDate && (BoardDate >= FileDate))
 	{
 		print("Attemp to flash outdated firmware")
 		return
 	}
-	
+
 	// Определение типа прошивки и запуск процесса
 	var MCUType = Firmware_DetectMCUType(FileName)
-	if(MCUType === "STM32")
+	if (MCUType === "STM32")
 		return Firmware_STM32(FileName)
-	else if(MCUType === "TMS")
+	else if (MCUType === "TMS")
 		return Firmware_TMS(FileName)
 	else
 		print("Unrecognizible firmware file")
@@ -80,28 +80,28 @@ function Firmware_Update(NodeID, FileName)
 function Firmware_DetectMCUType(FileName)
 {
 	var BinArray = loadbin(FileName)
-	
-	if(BinArray[0] == 0x00 && BinArray[1] == 0x80 && BinArray[2] == 0x00 && BinArray[3] == 0x20)
+
+	if (BinArray[0] == 0x00 && BinArray[1] == 0x80 && BinArray[2] == 0x00 && BinArray[3] == 0x20)
 		return "STM32"
-	else if(BinArray[0] == 0x02 && BinArray[BinArray.length - 1] == 0x03)
+	else if (BinArray[0] == 0x02 && BinArray[BinArray.length - 1] == 0x03)
 		return "TMS"
 }
 
 function Firmware_LoadAnyFile(FileName)
 {
 	var BinArray = loadbin(FileName)
-	
+
 	// STM32
-	if(BinArray[0] == 0x00 && BinArray[1] == 0x80 && BinArray[2] == 0x00 && BinArray[3] == 0x20)
+	if (BinArray[0] == 0x00 && BinArray[1] == 0x80 && BinArray[2] == 0x00 && BinArray[3] == 0x20)
 		return BinArray
-	
+
 	// TMS
-	else if(BinArray[0] == 0x02 && BinArray[BinArray.length - 1] == 0x03)
+	else if (BinArray[0] == 0x02 && BinArray[BinArray.length - 1] == 0x03)
 	{
 		var OutStr = ""
-		for(var i = 3; i < BinArray.length - 4; i++)
+		for (var i = 3; i < BinArray.length - 4; i++)
 			OutStr += String.fromCharCode(BinArray[i])
-		
+
 		return OutStr.replace(/\$A/g, "").split(/[\s, ]+/)
 	}
 }
@@ -109,51 +109,51 @@ function Firmware_LoadAnyFile(FileName)
 function Firmware_TMS(FileName)
 {
 	var fl_array = Firmware_LoadAnyFile(FileName)
-	if(!fl_array)
+	if (!fl_array)
 	{
 		print("File load error")
 		return
 	}
-	
+
 	var VAL_REGEXP = RegExp("^[0-9a-fA-F]{2}$")
-	var BLOCK_SIZE = 300		// Write buffer size
-	var ArrayIndex = 0			// Current array index
-	var WriteBuffer = []		// Transmit buffer
-	var Progress = 0			// For progress display
-	var XOR = 0					// XOR checksum
-	var Build16Trigger = 0		// Trigger for building 16bit values
-	var Build16Data = 0			// 16bit value
-	var Num16bParts = 0			// Number of uncompleted 16bit parts
-	
+	var BLOCK_SIZE = 300 // Write buffer size
+	var ArrayIndex = 0 // Current array index
+	var WriteBuffer = [] // Transmit buffer
+	var Progress = 0 // For progress display
+	var XOR = 0 // XOR checksum
+	var Build16Trigger = 0 // Trigger for building 16bit values
+	var Build16Data = 0 // 16bit value
+	var Num16bParts = 0 // Number of uncompleted 16bit parts
+
 	// reset device
 	dev.c(320)
 	sleep(500)
-	
+
 	print("Erasing SECTOR C...")
 	dev.c(302)
 	print("Erasing SECTOR D...")
 	dev.c(303)
-	
+
 	// reset counters
 	sleep(100)
 	dev.c(308)
-	
+
 	print("Writing data...")
 	while (ArrayIndex < fl_array.length)
-	{		
+	{
 		if (VAL_REGEXP.test(fl_array[ArrayIndex]))
 		{
 			// [data element]
-			
+
 			var curVal = parseInt(fl_array[ArrayIndex], 16)
-			
+
 			// display progress
 			if (ArrayIndex > ((fl_array.length / 10) * Progress))
 			{
 				print((Progress * 10) + "%")
 				Progress++
 			}
-			
+
 			// fill block buffer
 			if (WriteBuffer.length < BLOCK_SIZE)
 			{
@@ -169,7 +169,7 @@ function Firmware_TMS(FileName)
 					XOR ^= Build16Data
 					WriteBuffer.push(Build16Data)
 				}
-				
+
 				// only after processing value
 				ArrayIndex++
 			}
@@ -178,7 +178,7 @@ function Firmware_TMS(FileName)
 				// write to processor
 				dev.wa(1, WriteBuffer)
 				dev.c(310)
-				
+
 				// reset buffer
 				WriteBuffer = []
 			}
@@ -186,57 +186,59 @@ function Firmware_TMS(FileName)
 		else
 		{
 			// [address element]
-			
+
 			if (WriteBuffer.length > 0)
 			{
 				// write to processor
 				dev.wa(1, WriteBuffer)
 				dev.c(310)
-				
+
 				// for uncompleted operation
 				if (Build16Trigger) Num16bParts++
 				Build16Trigger = 0
-				
+
 				// reset buffer
 				WriteBuffer = []
 			}
-			
+
 			// set address
 			var curAddr = parseInt(fl_array[ArrayIndex], 16)
 			ArrayIndex++
-			
-			dev.w(2, curAddr & 0xffff)				// low part
-			dev.w(3, (curAddr >> 16) & 0xffff)		// high part
+
+			dev.w(2, curAddr & 0xffff) // low part
+			dev.w(3, (curAddr >> 16) & 0xffff) // high part
 			dev.c(309)
-			
+
 			XOR ^= curAddr & 0xffff
 			XOR ^= (curAddr >> 16) & 0xffff
 		}
 	}
-	
+
 	if (WriteBuffer.length > 0)
 	{
 		// write to processor
 		dev.wa(1, WriteBuffer)
 		dev.c(310)
-		
+
 		// reset buffer
 		WriteBuffer = []
 	}
-	
+
 	// diag output
 	if (Num16bParts)
 		print("Number of uncompleted words: " + Num16bParts)
-	
+
 	// compare XOR values
 	if (XOR == dev.r(10))
 	{
 		print("100%")
 		print("Checksum OK")
-		
+
 		dev.w(4, XOR)
 		dev.c(311)
-		
+
+		Firmware_WaitInit()
+
 		print("Firmware update completed")
 		return true
 	}
@@ -249,32 +251,32 @@ function Firmware_TMS(FileName)
 function Firmware_STM32(FileName)
 {
 	// Команды
-	var ACT_DEVICE_RESET = 320				// Команда перезапуска процессора
-	var ACT_FLASH_ERASE = 300				// Команда на очистку FLASH памяти процессора
-	var ACT_ALL_VARIABLES_RESET = 301		// Очитска всех переменных
-	var ACT_PROGRAMM_DATA = 302				// Команда на запись данных во FLASH память процессора
-	var ACT_CRC_CHECK_AND_RESET = 303		// Проверить CRC MCU и PC и перезагрузить процессор
+	var ACT_DEVICE_RESET = 320 // Команда перезапуска процессора
+	var ACT_FLASH_ERASE = 300 // Команда на очистку FLASH памяти процессора
+	var ACT_ALL_VARIABLES_RESET = 301 // Очитска всех переменных
+	var ACT_PROGRAMM_DATA = 302 // Команда на запись данных во FLASH память процессора
+	var ACT_CRC_CHECK_AND_RESET = 303 // Проверить CRC MCU и PC и перезагрузить процессор
 
 	// Адреса регистров
-	var REG_BYTE_NUM = 2					// Регистр количества байт
-	var REG_XOR_PC = 3						// Регистр контрольной суммы PC
-	var REG_XOR_MCU = 10					// Регистр контрольной суммы MCU
+	var REG_BYTE_NUM = 2 // Регистр количества байт
+	var REG_XOR_PC = 3 // Регистр контрольной суммы PC
+	var REG_XOR_MCU = 10 // Регистр контрольной суммы MCU
 
 	var fl_array = Firmware_LoadAnyFile(FileName)
-	if(!fl_array)
+	if (!fl_array)
 	{
 		print("File load error")
 		return
 	}
-	
-	var LocalIndex = 0						// Текущий индекс блока записываемых данных
-	var BLOCK_SIZE = 512					// Количество записываемых данных
-	var ArrayIndex = 0						// Текущий индекс массива
-	var Word_H = 0							// Старшая часть слова ячейки памяти
-	var Word_L = 0							// Младшая часть слова ячейки памяти
-	var WriteBuffer = []					// Буфер передачи
-	var Progress = 0						// Процесс программирования (проценты на экране)
-	var XOR = 0								// XOR контрольная сумма
+
+	var LocalIndex = 0 // Текущий индекс блока записываемых данных
+	var BLOCK_SIZE = 512 // Количество записываемых данных
+	var ArrayIndex = 0 // Текущий индекс массива
+	var Word_H = 0 // Старшая часть слова ячейки памяти
+	var Word_L = 0 // Младшая часть слова ячейки памяти
+	var WriteBuffer = [] // Буфер передачи
+	var Progress = 0 // Процесс программирования (проценты на экране)
+	var XOR = 0 // XOR контрольная сумма
 
 	print("Device reset.")
 	dev.c(ACT_DEVICE_RESET)
@@ -293,7 +295,7 @@ function Firmware_STM32(FileName)
 			print((Progress * 10) + "%")
 			Progress++
 		}
-		
+
 		// Отправка блока
 		if ((fl_array.length - ArrayIndex) >= BLOCK_SIZE * 2)
 		{
@@ -314,16 +316,16 @@ function Firmware_STM32(FileName)
 				ArrayIndex += 4
 				LocalIndex += 2
 			}
-			dev.w(REG_BYTE_NUM, LocalIndex)		// Передача количество байт
-			dev.wa(1, WriteBuffer)				// Передача данных
-			dev.c(ACT_PROGRAMM_DATA)			// Отправка команды на запись данных во FLASH память процессора
+			dev.w(REG_BYTE_NUM, LocalIndex) // Передача количество байт
+			dev.wa(1, WriteBuffer) // Передача данных
+			dev.c(ACT_PROGRAMM_DATA) // Отправка команды на запись данных во FLASH память процессора
 			LocalIndex = 0
 		}
 		// Передача оставшихся байт
 		else
 		{
 			WriteBuffer = []
-			
+
 			while (ArrayIndex < fl_array.length)
 			{
 				Word_L = fl_array[ArrayIndex] << 8
@@ -355,8 +357,13 @@ function Firmware_STM32(FileName)
 	{
 		print("100%")
 		print("Checksum OK")
+
 		dev.w(REG_XOR_PC, XOR)
 		dev.c(ACT_CRC_CHECK_AND_RESET)
+
+		Firmware_WaitInit()
+
+		print("Firmware update completed")
 		return true
 	}
 	else
@@ -368,5 +375,26 @@ function Firmware_STM32(FileName)
 		print("Erasing memory...")
 		dev.c(ACT_FLASH_ERASE)
 		print("The memory has been cleared!")
+	}
+}
+
+function Firmware_WaitInit()
+{
+	var sleep_time = 10
+	var timeout = 5000 / sleep_time
+	var counter = 0
+	while (counter < timeout)
+	{
+		try
+		{
+			// 23 - последний доступный регистр в загрузчике
+			dev.Read16Silent(24)
+			return
+		}
+		catch (e)
+		{
+			counter++
+			sleep(sleep_time)
+		}
 	}
 }
