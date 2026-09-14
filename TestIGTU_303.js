0eaeba82ecd677448ab5b8a50f4972f1e795ad46
A-Kuptsova <cuptsowa.n1997@gmail.com>
2026-09-14 18:20:55 +0300

    Доработки скрипта IGTU 303

--- TestIGTU_303.js
+++ TestIGTU_303.js
@@ -77,3 +77,36 @@ function IGTU_Vgs(Current)
 }
 //--------------------
 
+function IGTU_SaveCSV_EP()
+{
+	var Suffix = GetDateTimeSuffix();
+
+	save("data/IGTU_EP1_RegulatorIg_" + Suffix + ".csv", dev.raff(1));
+	save("data/IGTU_EP2_RegulatorUg_" + Suffix + ".csv", dev.raff(2));
+	save("data/IGTU_EP3_RegulatorUpot_" + Suffix + ".csv", dev.raff(3));
+	save("data/IGTU_EP4_RegulatorSetpoint_" + Suffix + ".csv", dev.raff(4));
+	save("data/IGTU_EP5_RegulatorCorrection_" + Suffix + ".csv", dev.raff(5));
+	save("data/IGTU_EP6_RegulatorError_" + Suffix + ".csv", dev.raff(6));
+	save("data/IGTU_EP7_DACRaw_" + Suffix + ".csv", dev.raff(7));
+	save("data/IGTU_EP20_ExtInfoData_" + Suffix + ".csv", dev.raff(20));
+}
+
+function GetDateTimeSuffix()
+{
+	var now = new Date();
+
+	var day = FormatTwoDigits(now.getDate());
+	var month = FormatTwoDigits(now.getMonth() + 1);
+	var year = now.getFullYear();
+
+	var hours = FormatTwoDigits(now.getHours());
+	var minutes = FormatTwoDigits(now.getMinutes());
+	var seconds = FormatTwoDigits(now.getSeconds());
+
+	return day + "." + month + "." + year + "_" + hours + "-" + minutes + "-" + seconds;
+}
+
+function FormatTwoDigits(value)
+{
+	return (value < 10 ? "0" : "") + value;
+}
